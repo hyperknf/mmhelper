5dd95cac7206f9944531300b14f89248f1f7e21c
hyperknf <superdooperalt@gmail.com>
2026-01-12 19:30:00 +0800

    updated for 1.21.5

--- src/main/java/com/hyperknf/mmhelper/MMHelper.java
+++ src/main/java/com/hyperknf/mmhelper/MMHelper.java
@@ -117,8 +117,6 @@ public class MMHelper implements ModInitializer {
         Config config = ConfigManager.getConfig();
         if (state != config.enabled)
             config.enabled = state;
-        else
-            logger.warn("MMHelper is already enabled");
     }
 
     public static void setHighlightMurders(boolean state) {
