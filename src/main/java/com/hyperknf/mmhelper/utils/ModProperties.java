d33b875f8bb9f8adc4d15806674e28faf40e0ae1
hyperknf <superdooperalt@gmail.com>
2026-01-15 18:32:38 +0800

    fixing stupid mistakes v1

--- src/main/java/com/hyperknf/mmhelper/utils/ModProperties.java
+++ src/main/java/com/hyperknf/mmhelper/utils/ModProperties.java
@@ -10,5 +10,5 @@ public class ModProperties {
     public static final ModMetadata METADATA = MinecraftUtils.getModMetadata(MOD_ID);
     public static final String MOD_NAME = METADATA.getName();
     public static final Version MOD_VERSION = new Version(METADATA.getVersion().getFriendlyString());
-    public static final Version MC_VERSION = new Version("1.20.4");
+    public static final Version MC_VERSION = new Version("1.21.5");
 }
